56503747ab942eade956b27d678b60ecb35e960b
Kirill20704 <73089058+Kirill20704@users.noreply.github.com>
2022-10-28 22:09:11 +0700

    Create Record.java

---
+++ Record.java
@@ -0,0 +1,14 @@
+package ex3;
+
+public class Record {
+    protected long id;
+    protected String phoneNumber;
+    protected String name;
+
+    public Record(long id, String phoneNumber, String name) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+}
